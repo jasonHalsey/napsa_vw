bbfea872bd03bd893c98d7b07448cd9386d0a0cb
JASON W HALSEY <jasonwhalsey@JASONs-MacBook-Pro.local>
2017-06-06 17:40:25 -0700

    Client changes #1

--- index.php
+++ index.php
@@ -8,9 +8,18 @@ Template Name: Index
 
 	<div id="primary" class="content-area">
 		<section id="header_video">
-			<iframe width="560" height="315" src="https://www.youtube.com/embed/32C685KDoFo?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
+			<!-- <iframe width="560" height="315" src="https://www.youtube.com/embed/32C685KDoFo?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe> -->
+			  <div class="videoWrapper videoWrapper169 js-videoWrapper">
+			    <!-- YouTube iframe. -->
+			    <!-- note the iframe src is empty by default, the url is in the data-src="" argument -->
+			    <!-- also note the arguments on the url, to autoplay video, remove youtube adverts/dodgy links to other videos, and set the interface language -->
+			    <iframe class="videoIframe js-videoIframe" src="" frameborder="0" allowTransparency="true" allowfullscreen data-src="https://www.youtube.com/embed/32C685KDoFo?autoplay=1&rel=0&amp;showinfo=0" ></iframe>
+			    <!-- the poster frame - in the form of a button to make it keyboard accessible -->
+			    <span class="videoPoster js-videoPoster" style="background-image:url(<?php echo get_stylesheet_directory_uri(); ?>/images/christopher_ape.jpg">&nbsp;</span>
+			  </div>
+  
 			<div class="video_info_bar">
-				Watch our video: Welcome to NAPSA Virtual Workshop 2017 (6:19)
+				Featured: Christopher, Center for Great Apes
 			</div>
 		</section>	
 		<main id="main" class="site-main" role="main">
@@ -28,7 +37,7 @@ Template Name: Index
 
 
 			<section class="videofeed napsa_member_sanctuaries">
-				<h2 id="napsa_member_sanctuaries">Learn more about NAPSA Member Sanctuaries</h2>
+				<h2 id="napsa_member_sanctuaries">NAPSA and its Members</h2>
 				<?php
 					// $napsa_member_sanctuaries = array( 'post_type' => 'napsavid', 'taxonomy' => 'napsa_member_sanctuaries', 'orderby' => 'menu_order');
 					// $loop = new WP_Query( $napsa_member_sanctuaries );
@@ -62,35 +71,7 @@ Template Name: Index
 				<?php wp_reset_query(); ?>
 			</section> <!-- .videofeed -->
 
-			<section class="videofeed sanctuary_retirement">
-				<h2 id="sanctuary_retirement">Sanctuary Retirement</h2>
-				<?php
-					$sanctuary_retirement = array( 'post_type' => 'napsavid', 'tax_query' =>
-                               array(
-                                  array(
-                                    'taxonomy' => 'tag',
-                                    'field'    => 'name',
-                                    'terms' => sanctuary_retirement,                                    
-                                  ),
-                                ), 
-                );
-					$loop = new WP_Query( $sanctuary_retirement );
-				?>
-				<?php while ( $loop->have_posts() ) : $loop->the_post();?>
-					<a class="post_block" href="<?php the_permalink(); ?>">
-						<div class="img_hold">
-							<div class="play_arrow"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/play_arrow.svg"></div>
-							<img src="<?php echo get_post_meta( $post->ID, '_cmb2_placholder_image', true ); ?>" class="placeholder" />
-						</div>
-						<div class="vid_info">
-							<h2><?php the_title() ?></h2>
-							<p><?php echo wpautop(get_post_meta( $post->ID, '_cmb2_short_description', true )); ?></p>
-						</div>
-					</a>
-				<?php endwhile; ?>
-
-				<?php wp_reset_query(); ?>
-			</section> <!-- .videofeed -->
+			
 
 
 			<section class="videofeed caregiving">
@@ -123,19 +104,19 @@ Template Name: Index
 				<?php wp_reset_query(); ?>
 			</section> <!-- .videofeed -->
 
-			<section class="videofeed facility_design">
-				<h2 id="facility_design">Facility Design</h2>
+			<section class="videofeed veterinary">
+				<h2 id="veterinary">Veterinary</h2>
 				<?php
-					$facility_design = array( 'post_type' => 'napsavid', 'tax_query' =>
+					$veterinary = array( 'post_type' => 'napsavid', 'tax_query' =>
                                array(
                                   array(
                                     'taxonomy' => 'tag',
                                     'field'    => 'name',
-                                    'terms' => facility_design,                                    
+                                    'terms' => veterinary,                                    
                                   ),
                                 ), 
                 );
-					$loop = new WP_Query( $facility_design );
+					$loop = new WP_Query( $veterinary );
 				?>
 				<?php while ( $loop->have_posts() ) : $loop->the_post();?>
 					<a class="post_block" href="<?php the_permalink(); ?>">
@@ -154,7 +135,7 @@ Template Name: Index
 			</section> <!-- .videofeed -->
 
 			<section class="videofeed sanctuary_management">
-				<h2 id="sanctuary_management">Sanctuary Management</h2>
+				<h2 id="sanctuary_management">Management</h2>
 				<?php
 					$sanctuary_management = array( 'post_type' => 'napsavid', 'tax_query' =>
                                array(
@@ -183,19 +164,19 @@ Template Name: Index
 				<?php wp_reset_query(); ?>
 			</section> <!-- .videofeed -->
 
-			<section class="videofeed collaboration">
-				<h2 id="collaboration">Collaboration</h2>
+			<section class="videofeed safety">
+				<h2 id="safety">Safety</h2>
 				<?php
-					$collaboration = array( 'post_type' => 'napsavid', 'tax_query' =>
+					$safety = array( 'post_type' => 'napsavid', 'tax_query' =>
                                array(
                                   array(
                                     'taxonomy' => 'tag',
                                     'field'    => 'name',
-                                    'terms' => collaboration,                                    
+                                    'terms' => safety,                                    
                                   ),
                                 ), 
                 );
-					$loop = new WP_Query( $collaboration );
+					$loop = new WP_Query( $safety );
 				?>
 				<?php while ( $loop->have_posts() ) : $loop->the_post();?>
 					<a class="post_block" href="<?php the_permalink(); ?>">
@@ -213,37 +194,7 @@ Template Name: Index
 				<?php wp_reset_query(); ?>
 			</section> <!-- .videofeed -->
 
-			<section class="videofeed supplemental_information">
-				<h2 id="supplemental_information">Supplemental Information</h2>
-				<?php
-					$supplemental_information = array( 'post_type' => 'napsavid', 'tax_query' =>
-                               array(
-                                  array(
-                                    'taxonomy' => 'tag',
-                                    'field'    => 'name',
-                                    'terms' => supplemental_information,                                    
-                                  ),
-                                ), 
-                );
-					$loop = new WP_Query( $supplemental_information );
-				?>
-				<?php while ( $loop->have_posts() ) : $loop->the_post();?>
-					<a class="post_block" href="<?php the_permalink(); ?>">
-						<div class="img_hold">
-							<div class="play_arrow"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/play_arrow.svg"></div>
-							<img src="<?php echo get_post_meta( $post->ID, '_cmb2_placholder_image', true ); ?>" class="placeholder" />
-						</div>
-						<div class="vid_info">
-							<h2><?php the_title() ?></h2>
-							<p><?php echo wpautop(get_post_meta( $post->ID, '_cmb2_short_description', true )); ?></p>
-						</div>
-					</a>
-				<?php endwhile; ?>
-
-				<?php wp_reset_query(); ?>
-			</section> <!-- .videofeed -->
-
-
+			
 
 		</main><!-- .site-main -->
 		<section id="sidebar_nap">
--- js/napsa_vw-custom.js
+++ js/napsa_vw-custom.js
@@ -1,4 +1,40 @@
 
 jQuery( document ).ready(function() {
-    jQuery('#side_info').prepend("<img id='monkey_icon' src='"+templateUrl+"/images/monkey_icon.png' />");
+    jQuery('#side_info').prepend("<img id='monkey_icon' src='"+templateUrl+"/images/chat_green.png' />");
 });
+
+
+
+// poster frame click event
+jQuery(document).on('click','.js-videoPoster',function(ev) {
+  ev.preventDefault();
+  var $poster = jQuery(this);
+  var $wrapper = $poster.closest('.js-videoWrapper');
+  videoPlay($wrapper);
+});
+
+// play the targeted video (and hide the poster frame)
+function videoPlay($wrapper) {
+  var $iframe = $wrapper.find('.js-videoIframe');
+  var src = $iframe.data('src');
+  // hide poster
+  $wrapper.addClass('videoWrapperActive');
+  // add iframe src in, starting the video
+  $iframe.attr('src',src);
+}
+
+// stop the targeted/all videos (and re-instate the poster frames)
+function videoStop($wrapper) {
+  // if we're stopping all videos on page
+  if (!$wrapper) {
+    var $wrapper = $('.js-videoWrapper');
+    var $iframe = $('.js-videoIframe');
+  // if we're stopping a particular video
+  } else {
+    var $iframe = $wrapper.find('.js-videoIframe');
+  }
+  // reveal poster
+  $wrapper.removeClass('videoWrapperActive');
+  // remove youtube link, stopping the video from playing in the background
+  $iframe.attr('src','');
+}
